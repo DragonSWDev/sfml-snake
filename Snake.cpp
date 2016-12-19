f3a789e23403c203d78cb93dbabbab92810c8ba8
BlackDragonK99 <kamils096@o2.pl>
2016-12-19 20:21:05 +0100

    Drawing snake body

--- Snake.cpp
+++ Snake.cpp
@@ -7,10 +7,15 @@ Snake::Snake(int x, int y)
     snakeHead.setSize(Vector2f(snakeSize,snakeSize));
     snakeHead.setFillColor(Color::Green);
     
+    snakeBodySegment.setSize(Vector2f(snakeSize, snakeSize));
+    snakeBodySegment.setFillColor(Color::Yellow);
+    
     snakeX = x;
     snakeY = y;
     
-    snakeHead.setPosition(snakeX*40,snakeY*40);
+    snakeHead.setPosition(snakeX*snakeSize,snakeY*snakeSize);
+    
+    snakeBody.push_back(DoubleVector2(x, y+1)); //Add one body segment behind head
 }
 
 int Snake::getX()
@@ -25,6 +30,13 @@ int Snake::getY()
 
 void Snake::drawSnake(RenderWindow& window)
 {
+    for(vector<DoubleVector2>::iterator i = snakeBody.begin(); i != snakeBody.end(); i++)
+    {
+        snakeBodySegment.setPosition(i->getX()*snakeSize, i->getY()*snakeSize);
+        window.draw(snakeBodySegment);
+    }
+        
+    
     window.draw(snakeHead);
 }
 
--- Snake.h
+++ Snake.h
@@ -1,10 +1,15 @@
 #ifndef SNAKE_H
 #define SNAKE_H
 
+#include <vector>
+
 #include <SFML/Graphics.hpp>
+
+#include "DoubleVector2.h"
 
 #define SNAKE_SIZE 40 //Snake is reactangle
 
+using namespace std;
 using namespace sf;
 
 class Snake
@@ -12,8 +17,10 @@ class Snake
 private:
     int snakeSize;
     int snakeX, snakeY; //Max 20 in X (800/40) and max 15 in Y (600/40)
+    vector <DoubleVector2> snakeBody;
     
     RectangleShape snakeHead;
+    RectangleShape snakeBodySegment;
     
 public:
     Snake(int x, int y); //Create and put Snake in desired position
