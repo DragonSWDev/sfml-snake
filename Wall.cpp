a8e6a85734c19cf349f21fc7c3f908251dcb482f
BlackDragonK99 <kamils096@o2.pl>
2016-12-24 18:24:57 +0100

    Improve wall generating

--- Wall.cpp
+++ Wall.cpp
@@ -3,7 +3,7 @@
 Wall::Wall(int size, int width, int height)
 {
     wallSize = size;
-    int maxParts = rand()%(20-5+1)+5;
+    int maxParts = rand()%(40-20+1)+20;
     
     int direction;
     int x=width/size/2, y=height/size/2;
@@ -21,21 +21,33 @@ Wall::Wall(int size, int width, int height)
             //Up
             case 1:
                y--;
+               
+               if(y < 1)
+                   y=height/size - 2;
             break;
             
             //Down
             case 2:
                 y++;
+                
+                if(y >= height/size - 2)
+                    y = 1;
             break;
             
             //Left
             case 3:
                 x--;
+                
+                if(x < 1)
+                    x = width/size - 2;
             break;
             
             //Right
             case 4:
                 x++;
+                
+                if(x >= width/size - 2)
+                    x = 1;
             break;
         }
         parts.push_back(Vector2f(x*size,y*size));
